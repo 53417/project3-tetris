40371c0eac8209cc3e686fd00cc8c83cf9685bff
53417 <seanimal@Live.com.au>
2020-01-16 21:28:50 +1100

    vertical + horizontal active piece movement working

--- web/src/things/sp_game/sp_game.js
+++ web/src/things/sp_game/sp_game.js
@@ -9,7 +9,9 @@ export default class Sp_game extends React.Component {
         this.state = {
             board: {},
             rows: 24,
-            cols: 10
+            cols: 10,
+            active_piece_name: '',
+            active_piece_direction: 0,
         };
     }
 
@@ -27,7 +29,7 @@ export default class Sp_game extends React.Component {
                 data[counter] = {
                     row: x,
                     col: y,
-                    state: "empty", //empty, filled, active
+                    state: "empty", //empty, full, active
                     line_top: false,
                     line_right: false,
                     line_bot: false,
@@ -502,7 +504,7 @@ export default class Sp_game extends React.Component {
             }
         };
 
-        //setState * 8
+        //setState * 8; active_cells set to empty, active_cells moved down 1 row (id + 10)
         this.setState({
             board: {
                 ...board,[active_keys[0]]: { 
@@ -553,7 +555,7 @@ export default class Sp_game extends React.Component {
                     line_right: active_cells[active_keys[0]].line_right,
                     line_bot: active_cells[active_keys[0]].line_bot,
                     line_left: active_cells[active_keys[0]].line_left,
-                    fill: active_cells[active_keys[0]].col
+                    fill: active_cells[active_keys[0]].fill
                 },
                 [active_keys[1] + 10]: {
                     row: board[active_keys[1] + 10].row,
@@ -563,7 +565,7 @@ export default class Sp_game extends React.Component {
                     line_right: active_cells[active_keys[1]].line_right,
                     line_bot: active_cells[active_keys[1]].line_bot,
                     line_left: active_cells[active_keys[1]].line_left,
-                    fill: active_cells[active_keys[1]].col
+                    fill: active_cells[active_keys[1]].fill
                 },
                 [active_keys[2] + 10]: {
                     row: board[active_keys[2] + 10].row,
@@ -573,7 +575,7 @@ export default class Sp_game extends React.Component {
                     line_right: active_cells[active_keys[2]].line_right,
                     line_bot: active_cells[active_keys[2]].line_bot,
                     line_left: active_cells[active_keys[2]].line_left,
-                    fill: active_cells[active_keys[2]].col
+                    fill: active_cells[active_keys[2]].fill
                 },
                 [active_keys[3] + 10]: {
                     row: board[active_keys[3] + 10].row,
@@ -583,7 +585,7 @@ export default class Sp_game extends React.Component {
                     line_right: active_cells[active_keys[3]].line_right,
                     line_bot: active_cells[active_keys[3]].line_bot,
                     line_left: active_cells[active_keys[3]].line_left,
-                    fill: active_cells[active_keys[3]].col
+                    fill: active_cells[active_keys[3]].fill
                 }
             }
         }, () => {
@@ -591,11 +593,186 @@ export default class Sp_game extends React.Component {
         });
     }
 
+    //moves cells eg: a1 to b1
+    update_static_cells(a1, a2, a3, a4, b1, b2, b3, b4) {
+        const { board } = this.state;
+        var active_cells = {
+            1: board[a1],
+            2: board[a2],
+            3: board[a3],
+            4: board[a4]
+        };
+
+        console.log(active_cells);
+        // setState * 8; active_cells set to empty, active_cells moved down 1 row (id + 10)
+        this.setState({
+            board: {
+                ...board,
+                [a1]: { 
+                    row: board[a1].row,
+                    col: board[a1].col,
+                    state: "empty",
+                    line_top: false,
+                    line_right: false,
+                    line_bot: false,
+                    line_left: false,
+                    fill: ""
+                },
+                [a2]: {
+                    row: board[a2].row,
+                    col: board[a2].col,
+                    state: "empty",
+                    line_top: false,
+                    line_right: false,
+                    line_bot: false,
+                    line_left: false,
+                    fill: ""
+                },
+                [a3]: {
+                    row: board[a3].row,
+                    col: board[a3].col,
+                    state: "empty",
+                    line_top: false,
+                    line_right: false,
+                    line_bot: false,
+                    line_left: false,
+                    fill: ""
+                },
+                [a4]: {
+                    row: board[a4].row,
+                    col: board[a4].col,
+                    state: "empty",
+                    line_top: false,
+                    line_right: false,
+                    line_bot: false,
+                    line_left: false,
+                    fill: ""
+                },
+                [b1]: { 
+                    row: board[b1].row,
+                    col: board[b1].col,
+                    state: "active",
+                    line_top: active_cells[1].line_top,
+                    line_right: active_cells[1].line_right,
+                    line_bot: active_cells[1].line_bot,
+                    line_left: active_cells[1].line_left,
+                    fill: active_cells[1].fill
+                },
+                [b2]: {
+                    row: board[b2].row,
+                    col: board[b2].col,
+                    state: "active",
+                    line_top: active_cells[2].line_top,
+                    line_right: active_cells[2].line_right,
+                    line_bot: active_cells[2].line_bot,
+                    line_left: active_cells[2].line_left,
+                    fill: active_cells[2].fill
+                },
+                [b3]: {
+                    row: board[b3].row,
+                    col: board[b3].col,
+                    state: "active",
+                    line_top: active_cells[3].line_top,
+                    line_right: active_cells[3].line_right,
+                    line_bot: active_cells[3].line_bot,
+                    line_left: active_cells[3].line_left,
+                    fill: active_cells[3].fill
+                },
+                [b4]: {
+                    row: board[b4].row,
+                    col: board[b4].col,
+                    state: "active",
+                    line_top: active_cells[4].line_top,
+                    line_right: active_cells[4].line_right,
+                    line_bot: active_cells[4].line_bot,
+                    line_left: active_cells[4].line_left,
+                    fill: active_cells[4].fill
+                }
+            }
+        }, () => {
+            this.board_render();
+        });
+    }
+
+    player_move_left() {
+        var active_keys = [];
+        const { board } = this.state;
+
+        //get active cells
+        for(var key in board) {
+            if(board[key].state === "active") {
+                active_keys.push(parseInt(key));
+            }
+        };
+
+        // - 1 for left
+        var a1 = active_keys[0];
+        var a2 = active_keys[1];
+        var a3 = active_keys[2];
+        var a4 = active_keys[3];
+        var b1 = active_keys[0] - 1;
+        var b2 = active_keys[1] - 1;
+        var b3 = active_keys[2] - 1;
+        var b4 = active_keys[3] - 1;
+
+        this.update_static_cells(a1, a2, a3, a4, b1, b2, b3, b4)
+    }
+
+    player_move_right() {
+        var active_keys = [];
+        const { board } = this.state;
+
+        //get active cells
+        for(var key in board) {
+            if(board[key].state === "active") {
+                active_keys.push(parseInt(key));
+            }
+        };
+
+        // - 1 for left
+        var a1 = active_keys[0];
+        var a2 = active_keys[1];
+        var a3 = active_keys[2];
+        var a4 = active_keys[3];
+        var b1 = active_keys[0] + 1;
+        var b2 = active_keys[1] + 1;
+        var b3 = active_keys[2] + 1;
+        var b4 = active_keys[3] + 1;
+
+        this.update_static_cells(a1, a2, a3, a4, b1, b2, b3, b4)
+    }
+
+    player_drop_piece() {
+
+    }
+
+    player_down_piece() {
+
+    }
+
+    // maybe have which piece is active in the state as well as its rotation position to determine cell updates
+    player_rotate_left() {
+
+    }
+
+    player_rotate_right() {
+
+    }
+
     //game board is 10 wide and 20 high
     // might make it 24 high to accomodate for beginning
-    // do a lose check everytime a new piece is spawned - if any values in first 4 rows = true then = lose
-    //new piece spawned auto when 1 piece is confirm dropped
+    // do a lose check everytime a new piece is placed - if any values in first 4 rows = true then = lose
+    // new piece spawned auto when 1 piece is confirm dropped and player hasnt lost yet
     // determine lines of pieces when being thrown down, ignore after placed to achieve cultris look
+    // confirm drop by looping each active piece id + 10, if all empty then continue cycle dropping, else set the piece to full and generate next piece
+
+    // 6 keys - required listeners
+    // rotate left - down arrow
+    // rotate right - up arrow
+    // move piece left - left arrow
+    // move piece right - right arrow
+    // move piece down 1 (need to reset active piece move down 1 row timer each time) - b key
+    // drop piece - spacebar
 
     render() {
         return(
@@ -614,7 +791,9 @@ export default class Sp_game extends React.Component {
             <Button variant="primary" onClick={() => this.piece_bks(5)}>input bks</Button>
             <br></br>
             <br></br>
+            <Button variant="primary" onClick={() => this.player_move_left()}>move left 1</Button>
             <Button variant="primary" onClick={() => this.active_down()}>active down 1</Button>
+            <Button variant="primary" onClick={() => this.player_move_right()}>move right 1</Button>
             <br></br>
             <br></br>
             <canvas id="canvas" width="250" height="600"></canvas>;
